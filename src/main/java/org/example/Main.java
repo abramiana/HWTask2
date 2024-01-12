50cb3cffe53916db591007f0771ef85afda81fab
kolobo4ok <142622191+kolobo4ok@users.noreply.github.com>
2024-01-13 00:53:24 +0200

    1. fix start Main class.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ import com.google.gson.Gson;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Gson().toJson(new User()));
+        System.out.println(new Gson().toJson(new User("Artur","Abramian")));
     }
 }
